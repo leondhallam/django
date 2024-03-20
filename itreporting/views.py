3f987717c82a9979a5f9c703e626b6657ae5781b
leondhallam <c2012047@hallam.shu.ac.uk>
2024-03-20 16:30:00 +0000

    Added CSS

--- itreporting/views.py
+++ itreporting/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'itreporting/home.html')
+    return render(request, 'itreporting/home.html', {'title': 'Welcome'})
 
 def about(request):
-    return render(request, 'itreporting/about.html')
+    return render(request, 'itreporting/about.html', {'title': 'About'})
 
 def contact(request):
-    return render(request, 'itreporting/contact.html')
+    return render(request, 'itreporting/contact.html', {'title': 'Contact'})
 # Create your views here.
