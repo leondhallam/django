05f4aad5c1ee8618ae98bedec7d496c872f52086
leondhallam <c2012047@hallam.shu.ac.uk>
2024-03-25 19:24:46 +0000

    Added extra stuff

--- users/views.py
+++ users/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-
+from django.contrib.auth.models import User
+from .forms import UserRegisterForm
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        form == UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save(commit=False)
+            user.is_staff = True  # Set the user as staff
+            user.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
+            messages.success(request, f'Your account has been created! Now you can login!')
+            return redirect('login')
+
         else:
             messages.warning(request, 'Unable to create account!')
-        return redirect('itreporting:home')
     else:
-        form = UserCreationForm()
-        return render(request, 'users/register.html', {'form': form , 'title': 'Student Registration'})
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form, 'title': 'Student Registration'})
